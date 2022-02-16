0a2dc35cb2ee2771b51852eeb153980f6b3d4e4f
ZDubeau <zdubeau@dubeaul.com>
2022-02-16 16:51:27 +0100

    Create login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AdminComponent } from './admin/admin.component';
+import { FranshiseComponent } from './franshise/franshise.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'franshise', component: FranshiseComponent },
+  //{ path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+  /**
+   * Otherwise redirect to home
+   * @url https://angular.io/guide/router
+   */
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/environments/environment.stage.ts
@@ -0,0 +1,6 @@
+// https://angular.io/guide/build
+
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost/4100'
+  };
