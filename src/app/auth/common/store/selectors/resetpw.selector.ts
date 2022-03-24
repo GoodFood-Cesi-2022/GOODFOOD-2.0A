43f52851ba1743ef45b96977c020c4d15e02113f
ZDubeau <zdubeau@dubeaul.com>
2022-03-24 11:51:44 +0100

    Remove resetPW

--- src/app/auth/common/store/selectors/resetpw.selector.ts
+++
@@ -1,9 +0,0 @@
-import { createFeatureSelector, createSelector } from "@ngrx/store";
-import { User } from "../../models/user";
-
-export const identityFeatureSelector = createFeatureSelector<User>('identity');
-
-export const isSubmittingSelector = createSelector(
-    identityFeatureSelector,
-    (identityState: User) => identityState["isSubmitting"]
-  );
