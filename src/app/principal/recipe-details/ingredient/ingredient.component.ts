6ab06fd71b68a0b96d1582317f90cf42f8a6b155
ZDubeau <bhr.sdqi@gmail.com>
2022-06-16 09:35:07 +0200

    Finish create new ingredient part

--- src/app/principal/recipe-details/ingredient/ingredient.component.ts
+++ src/app/principal/recipe-details/ingredient/ingredient.component.ts
@@ -1,11 +1,9 @@
-import { TypeVisitor } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import {
   FormGroup,
   Validators,
   FormControl,
   FormBuilder,
-  FormArray,
 } from '@angular/forms';
 import { Ingredient, IngreType } from 'src/app/shared/models/ingredient.model';
 import { RecipeService } from 'src/app/shared/services/recipe/recipe.service';
@@ -34,36 +32,26 @@ export class IngredientComponent implements OnInit {
     /* retrieve ingredient types */
     this.recipeService.getIngredientsTypes().subscribe((res) => {
       this.typeArray = res;
-      console.log('---> component ---> ingredient type : ', res);
     });
   }
 
   initForm(): void {
     this.form = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(3)]],
-      allergen: new FormControl(),
-      typeIngredient: new FormControl(),
-      // typeDetails: this.fb.array([
-      //   this.fb.group({
-      //     typeIngredient: new FormControl(),
-      //     description: [''],
-      //   }),
-      // ]),
-      // infoType: this.fb.group({
-      //   typeIngredient: new FormControl(),
-      //   description: ['', Validators.minLength(300)],
-      // }),
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.pattern('[a-zA-Z]'),
+        ],
+      ],
+      allergen: false,
+      typeIngredient: new FormControl([]),
     });
-    console.log('forn init : ', this.form);
   }
 
   isAllergen(event: any): void {
-    // console.log(
-    //   'ingredient component -> isAllergen -> event.checked',
-    //   event.checked
-    // );
     this.allergen = event.checked;
-    // this.ingredient.allergen = event.checked;
     if (event.checked) {
       this.allergen = true;
       this.form.get('allergen').setValue(true);
@@ -72,29 +60,16 @@ export class IngredientComponent implements OnInit {
     }
   }
 
-  /**
-   * tableau qui contient la liste des intermediaire dans le formulaire
-   * @returns {FormArray}
-   */
-  get typeDetails(): FormArray {
-    return this.form.controls['typeDetails'] as FormArray;
-  }
-
   onSubmit(): void {
-    //if (this.form.valid) {
-    //this.makeIngredient();
     const ingredient: Ingredient = {
-      name: this.form.get('title').value,
+      name: this.form.get('name').value,
       allergen: this.form.get('allergen').value,
-      types: this.form.get('typeIngredient').value,
+      types: [this.form.controls['typeIngredient'].value.code],
     };
-    console.log('res : ', ingredient);
+    console.log('res res : ', ingredient);
     this.recipeService
-      .createIngredient(this.ingredient)
+      .createIngredient(ingredient)
       .pipe()
       .subscribe((res) => console.log('res : ', res));
-    // }
   }
-
-  public onClose(): void {}
 }
