154eb172c145bbd833e018c146993092619a48f6
ZDubeau <bhr.sdqi@gmail.com>
2022-06-23 17:56:59 +0200

    schedule  model

---
+++ src/app/shared/models/schedule.model.ts
@@ -0,0 +1,16 @@
+export interface Schedule {
+  monday_lunch: Date;
+  monday_night: Date;
+  tuesday_lunch: Date;
+  tuesday_night: Date;
+  wednesday_lunch: Date;
+  wednesday_night: Date;
+  thursday_lunch: Date;
+  thursday_night: Date;
+  friday_lunch: Date;
+  friday_night: Date;
+  staturday_lunch: Date;
+  saturday_night: Date;
+  sunday_lunch: Date;
+  sunday_night: Date;
+}
