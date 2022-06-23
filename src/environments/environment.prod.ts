5f0415a53334e3ea01d7757b89af831b5b944039
ZDubeau <bhr.sdqi@gmail.com>
2022-06-23 18:22:38 +0200

    Try to fix github actions error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    production: true,
-    apiUrl: 'http://localhost/4000'
-  };
+  production: true,
+  apiUrl: 'http://localhost/4000',
+  apiBaseUrl: 'http://localhost:8080/api',
+};
