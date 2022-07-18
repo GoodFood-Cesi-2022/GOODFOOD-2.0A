bb87aa4530e0b940ee34ad09441aece25f85af2a
ZDubeau <zdubeau@dubeaul.com>
2022-07-18 22:55:13 +0200

    Clean larma file

--- karma.conf.js
+++ karma.conf.js
@@ -23,11 +23,6 @@ module.exports = function (config) {
       },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    // coverageIstanbulReporter: {
-    //   dir: require("path").join(__dirname, "./coverage/lcov-report"),
-    //   reports: ["json-summary", "html", "lcovonly"],
-    //   fixWebpackSourcePaths: true,
-    // },
     jasmineHtmlReporter: {
       suppressAll: true, // removes the duplicated traces
     },
