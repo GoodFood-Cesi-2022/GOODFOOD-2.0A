de9850fd5838472167f041fd1eab3073ffea756f
ZDubeau <zdubeau@dubeaul.com>
2022-07-24 17:48:22 +0200

    Add length of recipe, user, ingredient & franchisee

--- src/app/principal/home/home.component.spec.ts
+++ src/app/principal/home/home.component.spec.ts
@@ -1,21 +1,52 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { HomeComponent } from "./home.component";
+
+import { _HttpRequest } from "src/app/shared/constants/httpRequest.const";
+import { environment } from "src/environments/environment";
+import { mockUser1 } from "src/app/shared/mock/users.mock";
+import { mockFranchisee } from "src/app/shared/mock/franchisee.mock";
+import { mockIngredientArray } from "src/app/shared/mock/ingredients.mock";
 
 describe("HomeComponent", () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [HomeComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
+    httpTestingController = TestBed.inject(HttpTestingController);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    const req1 = httpTestingController.expectOne(`${environment.apiBaseUrl}/users`);
+    expect(req1.request.method).toEqual(_HttpRequest.GET);
+    req1.flush(mockUser1);
+
+    const req2 = httpTestingController.expectOne(`${environment.apiBaseUrl}/contractors`);
+    expect(req2.request.method).toEqual(_HttpRequest.GET);
+
+    // Respond with the mock address
+    req2.flush(mockFranchisee);
+
+    const req3 = httpTestingController.expectOne(`${environment.apiBaseUrl}/recipes?includes[]=pictures`);
+    expect(req3.request.method).toEqual(_HttpRequest.GET);
+    req3.flush({});
+
+    const req4 = httpTestingController.expectOne(`${environment.apiBaseUrl}/ingredients`);
+    expect(req4.request.method).toEqual(_HttpRequest.GET);
+    req4.flush({ data: mockIngredientArray });
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it("should create", () => {
--- src/app/principal/home/home.component.ts
+++ src/app/principal/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import { LoadingService } from "src/app/shared/services/loading/loading.service";
+import { UsersService } from "src/app/shared/services/users/users.service";
+import { finalize } from "rxjs/operators";
+import { FranchiseeService } from "src/app/shared/services/franchisee/franchisee.service";
+import { RecipeService } from "src/app/shared/services/recipe/recipe.service";
+import { IngredientService } from "src/app/shared/services/ingredient/ingredient.service";
 
 @Component({
   selector: "app-home",
@@ -6,11 +12,45 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-  constructor() {
+  userNumber: number;
+  franchiseeNumber: number;
+  recipeNumber: number;
+  ingredientNumber: number;
+
+  constructor(
+    private usersService: UsersService,
+    private loading: LoadingService,
+    private franchiseeService: FranchiseeService,
+    private recipeService: RecipeService,
+    private ingredientService: IngredientService
+  ) {
     //NOSONAR
   }
 
   ngOnInit(): void {
-    //NOSONAR
+    this.userNumber = 0;
+    this.franchiseeNumber = 0;
+    this.recipeNumber = 0;
+    this.ingredientNumber = 0;
+    this.loading.loadingOn();
+
+    this.usersService.getUsers().subscribe((res) => {
+      this.userNumber = res.length;
+    });
+
+    this.franchiseeService.getFranchisees().subscribe((res): void => {
+      this.franchiseeNumber = res.length;
+    });
+
+    this.recipeService.getRecipes().subscribe((res): void => {
+      this.recipeNumber = res.length;
+    });
+
+    this.ingredientService
+      .getIngredients()
+      .pipe(finalize((): void => this.loading.loadingOff()))
+      .subscribe((res) => {
+        this.ingredientNumber = res.length;
+      });
   }
 }
